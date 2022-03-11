86bd540975bec56a50cc301e1ceb53482ccafa0e
FelipeBs <felipe.bsilvaa@gmail.com>
2022-03-11 16:18:23 -0300

    Higher order function

---
+++ higherOrderFunction-2.py
@@ -0,0 +1,15 @@
+# Higher order function = a function that either:
+#                         1. accepts a function as an argument
+#                            or
+#                         2. returns a function
+#                         (In Python, functions are also treated as objects)
+
+
+def divisor(x):
+    def dividend(y):
+        return y / x
+    return dividend
+
+
+divide = divisor(2)
+print(divide(10))
